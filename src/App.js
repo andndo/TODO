929afc276a6c04cda5ea2326fd97470d48e3b3d0
anda <ando6160@gmail.com>
2022-03-21 17:15:57 +0900

    fix bug

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import React, { useState, useRef, useEffect } from "react";
 
 function App() {
   const [todo, setTodo] = useState("");
-  const [todos, setTodos] = useState(
-    () =>
-      JSON.parse(window.localStorage.getItem("todos")) || {
-        id: 0,
-        value: "",
-      }
-  );
+  const [todos, setTodos] = useState([
+    {
+      id: 0,
+      value: "asfd",
+    },
+  ]);
+
   const onChange = (event) => {
     setTodo(event.target.value);
   };
@@ -22,7 +22,7 @@ function App() {
     setTodos(todos.filter((todo) => todo.id !== id));
   };
 
-  function handleRemove(e, index) {
+  function handleRemove(index) {
     console.log(index);
     console.log(todos);
     const data = todos.filter((data) => data.id !== index);
@@ -34,15 +34,14 @@ function App() {
     if (todo === "") {
       return;
     }
-
     setTodo("");
     const data = {
       id: nextId.current,
       value: todo,
     };
-
-    setTodos((currentArray) => [...currentArray, data]);
+    
     nextId.current += 1;
+    setTodos((currentArray) => [...currentArray, data]);
 
     console.log(todos);
   };
@@ -51,7 +50,7 @@ function App() {
     setTodos(todos.filter((data) => data.id >= 100));
   };
 
-  const nextId = useRef(0);
+  const nextId = useRef(1);
 
   return (
     <div class="main">
@@ -65,20 +64,36 @@ function App() {
           onChange={onChange}
           class="input"
         />
-        <button style={{
-          backgroundColor:"blue",
-          color: "white",
-          borderRadius: 5, 
-          marginLeft: 4
-        }}>추가하기</button>
-        <button onClick={handleReset} class="resetBtn">리셋</button>
-        <hr style={{width: '100%', height: '1px', marginTop: "10px",backgroundColor: "black"}}/>
-        <div style={{ display: "flex", flexDirection: "column" }} class="contents">
-          {todos.map((item, index) => (
+        <button
+          style={{
+            backgroundColor: "blue",
+            color: "white",
+            borderRadius: 5,
+            marginLeft: 4,
+          }}
+        >
+          추가하기
+        </button>
+        <button onClick={handleReset} class="resetBtn">
+          리셋
+        </button>
+        <hr
+          style={{
+            width: "100%",
+            height: "1px",
+            marginTop: "10px",
+            backgroundColor: "black",
+          }}
+        />
+        <div
+          style={{ display: "flex", flexDirection: "column" }}
+          class="contents"
+        >
+          {todos.map((item) => (
             <ul style={{ display: "inline-flex" }}>
-              <li key={index}>{item.value}</li>
+              <li>{item.value}</li>
               <button
-                onClick={(e) => handleRemove(e, index)}
+                onClick={(e) => handleRemove(item.id)}
                 style={{
                   border: 0,
                   outline: "none",
